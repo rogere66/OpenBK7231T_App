2fb7970d1c51c6606a7b07b064460bfaa5fc8270
rogere66 <roger.e66@gmail.com>
2022-11-09 15:31:00 +0100

    Update hal_pins_win32.c

--- src/hal/win32/hal_pins_win32.c
+++ src/hal/win32/hal_pins_win32.c
@@ -41,7 +41,7 @@ void HAL_PIN_PWM_Stop(int pinIndex) {
 void HAL_PIN_PWM_Start(int index) {
 
 }
-void HAL_PIN_PWM_Update(int index, int value) {
+void HAL_PIN_PWM_Update(int index, float value) {
 
 
 }
