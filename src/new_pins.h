c6406af16242b65e676a65e85ace49ff10c3a638
rogere66 <roger.e66@gmail.com>
2022-11-09 15:34:18 +0100

    Update new_pins.h

--- src/new_pins.h
+++ src/new_pins.h
@@ -285,7 +285,7 @@ void CHANNEL_DoSpecialToggleAll();
 bool CHANNEL_Check(int ch);
 void PIN_SetGenericDoubleClickCallback(void (*cb)(int pinIndex));
 // CHANNEL_SET_FLAG_*
-void CHANNEL_Set(int ch, int iVal, int iFlags);
+void CHANNEL_Set(int ch, float iVal, int iFlags);
 void CHANNEL_Add(int ch, int iVal);
 void CHANNEL_AddClamped(int ch, int iVal, int min, int max);
 int CHANNEL_Get(int ch);
